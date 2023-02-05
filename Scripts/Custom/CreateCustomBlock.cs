c966389c3063761f51b688674fdd6d5f3283ffb5
songro_ <sdbb2q@gmail.com>
2023-02-05 18:06:34 +0900

    주석되있는 부분 고쳐야하는거

--- Scripts/Custom/CreateCustomBlock.cs
+++ Scripts/Custom/CreateCustomBlock.cs
@@ -1,14 +1,28 @@
 using UnityEngine;
 using UnityEngine.UI;
+using System.IO;
+
+public static class ConvertToSprite{
+    public static Sprite Convert2Sprite(this Texture2D texture){
+        return Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height), Vector2.zero);
+    }
+}
 
 public class CreateCustomBlock : MonoBehaviour
 {
     public Toggle addGravity;
     public Toggle addCollider;
     public Toggle setCircle;
-    public RawImage img;
+    private int limit = 99999999;
     BoxCollider2D bc;
     SpriteRenderer sr;
+    FileBrowserUpdate fbu;
+    public Sprite defsp;
+    public RawImage img;
+
+    void Start() {
+        fbu = GameObject.Find("TextureLoadManager").GetComponent<FileBrowserUpdate>();
+    }
 
     public void create() {
         GameObject customObject = new GameObject();
@@ -16,25 +30,29 @@ public class CreateCustomBlock : MonoBehaviour
         customObject.tag = "CustomBlock";
         customObject.AddComponent<SpriteRenderer>();
         customObject.AddComponent<RemoveCustom>();
+        limit--;
+        Debug.Log("CustomBlock Left: " + limit);
 
         sr = customObject.GetComponent<SpriteRenderer>();
-        // need to fix
-        // sr.sprite = img.texture;
+        //sr.sprite = ConvertToSprite.Convert2Sprite(img.texture);
 
         if(addGravity.isOn) {
             customObject.AddComponent<Rigidbody2D>();
-        }
+            Debug.Log("rigidbody2d = on");
+        } else {Debug.Log("rigidBody2d = off");}
 
         if(addCollider.isOn) {
             customObject.AddComponent<BoxCollider2D>();
             bc = customObject.GetComponent<BoxCollider2D>();
             bc.size = new Vector2(1f, 1f);
             Debug.Log("customBlock boxcollider size: " + bc.size);
-        }
+            Debug.Log("boxCollider2D = on");
+        } else {Debug.Log("rigidbody2d = off");}
 
         if(setCircle.isOn) {
             customObject.AddComponent<CircleCollider2D>();
-        }
+            Debug.Log("circleCollider2D = on");
+        } else {Debug.Log("circleCollider2D = off");}
 
     }
 }
